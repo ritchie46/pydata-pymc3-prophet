0410b00f4fa77384c345de644d9dfa0f2e8c00e2
ritchie46 <ritchie46@gmail.com>
2019-04-24 15:49:37 +0200

    local mathjax

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo downloading mathjax
+curl https://codeload.github.com/mathjax/MathJax/zip/master -o mathjax.zip
+unzip mathjax.zip
+
+echo downloading reveal.js
 git clone https://github.com/hakimel/reveal.js.git
 cd reveal.js
 git reset --hard 33bed47daca3f08c396215415e6ece005970734a
